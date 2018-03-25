414f9e3cdc2749b5984f443df14e9393a8c751f0
Dan Kelly <dankelly.6@gmail.com>
2018-03-25 08:13:55 -0400

    Refactor potentially troublesome time logic into separate functions

--- jasmine/src/Babysitter.js
+++ jasmine/src/Babysitter.js
@@ -6,7 +6,6 @@ function Babysitter(arrival, departure, bedtime){
 
     this.arrival = arrival;
     this.departure = departure;
-    this.departureOn24HourClock = (departure <= this.LATEST_DEPARTURE_TIME ? departure + 12 : departure);
     this.bedtime = bedtime;
 
     this.WATCHING_KIDS_RATE = 10;
@@ -17,20 +16,28 @@ function Babysitter(arrival, departure, bedtime){
         return this.getBasePay() - this.adjustPayForSleepingKids() - this.adjustPayForStayingAfterMidnight();
     };
 
+    this.stayedAfterMidnight = function(){
+        return this.departure <= this.LATEST_DEPARTURE_TIME;
+    };
+
+    this.getDepartureTimeOn24HourClock = function(){
+        return this.stayedAfterMidnight() ? this.departure + 12 : this.departure;
+    };
+
     this.getBasePay = function(){
-        var billableHours = this.departureOn24HourClock - this.arrival;
+        var billableHours = this.getDepartureTimeOn24HourClock() - this.arrival;
         return billableHours * this.WATCHING_KIDS_RATE;
     };
 
     this.adjustPayForSleepingKids = function(){
-        var lastPossibleHourAtAfterBedtimeRate = Math.min(this.departureOn24HourClock, this.LATEST_BEDTIME);
+        var lastPossibleHourAtAfterBedtimeRate = Math.min(this.getDepartureTimeOn24HourClock(), this.LATEST_BEDTIME);
         var potentialHoursBilledAtAfterBedtimeRate = lastPossibleHourAtAfterBedtimeRate - this.bedtime;
         var billableHoursAfterBedtime = (potentialHoursBilledAtAfterBedtimeRate > 0 ? potentialHoursBilledAtAfterBedtimeRate : 0);
         return (this.WATCHING_KIDS_RATE - this.KIDS_IN_BED_RATE) * billableHoursAfterBedtime;
     };
 
     this.adjustPayForStayingAfterMidnight = function(){
-        var billableHoursAfterMidnight = (this.departure <= this.LATEST_DEPARTURE_TIME ? this.departure : 0);
+        var billableHoursAfterMidnight = (this.stayedAfterMidnight() ? this.departure : 0);
         return (this.WATCHING_KIDS_RATE - this.AFTER_MIDNIGHT_RATE) * billableHoursAfterMidnight;
     };
 
