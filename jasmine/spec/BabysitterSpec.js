c1bda4f59b7dd0695a2e80d76083b57104bc7be2
Dan Kelly <dankelly.6@gmail.com>
2018-03-25 09:27:20 -0400

    Refactor tests and provide some extreme cases

--- jasmine/spec/BabysitterSpec.js
+++ jasmine/spec/BabysitterSpec.js
@@ -11,9 +11,9 @@
  the latest she can stay is 4am.
  */
 
-describe("Babysitter", function() {
+describe("A babysitter", function() {
 
-    describe("watches children for two hours before bedtime", function () {
+    describe("who watches children for two hours before bedtime", function () {
         var babysitter = new Babysitter();
         var result = babysitter.calculatePay(6, 8, 9);
          it("costs $20", function(){
@@ -21,23 +21,63 @@ describe("Babysitter", function() {
         });
     });
 
-    describe("arrives at 5 and watches children until bedtime at 8", function(){
+    describe("who watches children for three hours right up until bedtime", function () {
         var babysitter = new Babysitter();
-        var result = babysitter.calculatePay(5, 8, 8);
+        var result = babysitter.calculatePay(6, 9, 9);
         it("costs $30", function(){
             expect(result).toBe(30);
         });
     });
 
-    describe("arrives at 8, leaves at 2am, bedtime was 10", function(){
+    describe("who arrives at 9pm bedtime and stays until midnight", function(){
         var babysitter = new Babysitter();
-        var result = babysitter.calculatePay(8, 2, 10);
-        it("costs $48", function(){
-            expect(result).toBe(48);
+        var result = babysitter.calculatePay(9, 12, 9);
+        it("costs $18", function(){
+            expect(result).toBe(18);
         });
     });
 
-    describe("arrives at 6, leaves at 3am, bedtime was midnight", function(){
+    describe("who arrives at midnight and stays until 4am", function(){
+        var babysitter = new Babysitter();
+        var result = babysitter.calculatePay(12, 4, 5);
+        it("costs $32", function(){
+            expect(result).toBe(32);
+        });
+    });
+
+    describe("who watches children for three hours before bedtime and stays one hour after pre-midnight bedtime", function(){
+        var babysitter = new Babysitter();
+        var result = babysitter.calculatePay(7, 11, 10);
+        it("costs $36", function(){
+            expect(result).toBe(36);
+        });
+    });
+
+    describe("who watches children for one hour before midnight bedtime and stays four hours after midnight", function(){
+        var babysitter = new Babysitter();
+        var result = babysitter.calculatePay(11, 4, 12);
+        it("costs $42", function(){
+            expect(result).toBe(42);
+        });
+    });
+
+    describe("who watches children for four hours before bedtime and stays one hour after pre-midnight bedtime and 2 hours after midnight", function(){
+        var babysitter = new Babysitter();
+        var result = babysitter.calculatePay(7, 2, 11);
+        it("costs $62", function(){
+            expect(result).toBe(62);
+        });
+    });
+
+    describe("who arrives at the earliest possible time, puts kids to bed an hour later, and stays until the latest possible time", function(){
+        var babysitter = new Babysitter();
+        var result = babysitter.calculatePay(5, 4, 6);
+        it("costs $78", function(){
+            expect(result).toBe(78);
+        });
+    });
+
+    describe("who watches children for six hours before midnight bedtime and leaves at 3am", function(){
         var babysitter = new Babysitter();
         var result = babysitter.calculatePay(6, 3, 12);
         it("costs $84", function(){
