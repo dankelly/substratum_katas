462d6d38a4ef6b893502ce85c7502f54dc2d24c8
Dan Kelly <dankelly.6@gmail.com>
2018-03-25 01:59:24 -0400

    Add logic to fix failing test

--- jasmine/src/Babysitter.js
+++ jasmine/src/Babysitter.js
@@ -1,7 +1,33 @@
 function Babysitter(arrival, departure, bedtime){
 
+    this.EARLIEST_ARRIVAL_TIME = 5;
+    this.LATEST_BEDTIME = 12;
+    this.LATEST_DEPARTURE_TIME = 4;
+
+    this.arrival = arrival;
+    this.departure = departure;
+    this.departureOn24HourClock = (departure <= this.LATEST_DEPARTURE_TIME ? departure + 12 : departure);
+    this.bedtime = bedtime;
+
+    this.WATCHING_KIDS_RATE = 10;
+    this.KIDS_IN_BED_RATE = 6;
+    this.AFTER_MIDNIGHT_RATE = 8;
+
     this.calculatePay = function(){
-        return 20;
+        var billableHours = this.departureOn24HourClock - this.arrival;
+        var basePay = billableHours * this.WATCHING_KIDS_RATE;
+
+        var lastPossibleHourAtAfterBedtimeRate = Math.min(this.departureOn24HourClock, this.LATEST_BEDTIME);
+        var potentialHoursBilledAtAfterBedtimeRate = lastPossibleHourAtAfterBedtimeRate - this.bedtime;
+        var billableHoursAfterBedtime = (potentialHoursBilledAtAfterBedtimeRate > 0 ? potentialHoursBilledAtAfterBedtimeRate : 0);
+        var kidsInBedAdjustment = (this.WATCHING_KIDS_RATE - this.KIDS_IN_BED_RATE) * billableHoursAfterBedtime;
+
+        var billableHoursAfterMidnight = (this.departure <= this.LATEST_DEPARTURE_TIME ? this.departure : 0);
+        var afterMidnightAdjustment = (this.WATCHING_KIDS_RATE - this.AFTER_MIDNIGHT_RATE) * billableHoursAfterMidnight;
+
+        return basePay - kidsInBedAdjustment - afterMidnightAdjustment;
+
+
     }
 
 }
