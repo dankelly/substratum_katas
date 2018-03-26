a5b48ce8d6e220fd189c6c7493d01781c5357348
Dan Kelly <dankelly.6@gmail.com>
2018-03-25 23:46:29 -0400

    Improve sorting algorithm and remove excess code

--- jasmine/src/Closest.js
+++ jasmine/src/Closest.js
@@ -4,47 +4,11 @@ var Closest = {
         if(array.includes(0)) return 0;
 
         array.sort(function(x, y){
-            if(x < y) return -1;
-            if(y < x) return 1;
+            if(Math.abs(x) < Math.abs(y)) return -1;
+            if(Math.abs(y) < Math.abs(x)) return 1;
             return 0;
         });
-
-        var negativeCandidate, positiveCandidate, previousCandidate,
-            negativeIndex = 0,
-            positiveIndex = array.length - 1;
-
-        if (array[negativeIndex] < 0) {
-            negativeCandidate = array[negativeIndex];
-            negativeIndex += 1;
-            while (negativeCandidate < 0 && array[negativeIndex]) {
-                previousCandidate = negativeCandidate;
-                negativeCandidate = array[negativeIndex];
-                negativeIndex += 1;
-            }
-            if(array.length > 1) negativeCandidate = previousCandidate;
-            previousCandidate = undefined;
-        }
-
-        if(array[positiveIndex] > 0){
-            positiveCandidate = array[positiveIndex];
-            positiveIndex -= 1;
-            while (positiveCandidate > 0 && array[positiveIndex]) {
-                previousCandidate = positiveCandidate;
-                positiveCandidate = array[positiveIndex];
-                positiveIndex -= 1;
-            }
-            if(array.length > 1) positiveCandidate = previousCandidate;
-            previousCandidate = undefined;
-        }
-
-        if(!negativeCandidate) return positiveCandidate;
-        if(!positiveCandidate) return negativeCandidate;
-
-        var negativeDistanceFromZero = 0 - negativeCandidate;
-        var positiveDistanceFromZero = positiveCandidate - 0;
-
-        if(negativeDistanceFromZero < positiveDistanceFromZero) return negativeCandidate;
-        return positiveCandidate;
-
+        if(array[0] < 0 && array.includes(-array[0])) return -array[0];
+        return array[0];
     }
 };
