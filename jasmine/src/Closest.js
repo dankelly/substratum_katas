9a8bf27bea9c241c058a05eaa8746b56d1e3ae3b
Dan Kelly <dkelly@columbus.k12.oh.us>
2018-03-26 08:01:30 -0400

    Really take charge of the sorting function

--- jasmine/src/Closest.js
+++ jasmine/src/Closest.js
@@ -1,14 +1,11 @@
 var Closest = {
     toZero: function(array) {
         if (array.length === 0) throw "List must contain at least one value";
-        if(array.includes(0)) return 0;
 
         array.sort(function(x, y){
-            if(Math.abs(x) < Math.abs(y)) return -1;
-            if(Math.abs(y) < Math.abs(x)) return 1;
-            return 0;
+            return (Math.abs(x) - Math.abs(y)) || (x > 0 ? -1 : 1);
         });
-        if(array[0] < 0 && array.includes(-array[0])) return -array[0];
+
         return array[0];
     }
 };
