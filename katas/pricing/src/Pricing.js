a4c241785ea2a1f6e943ba08521a7493ef9c8f08
Dan Kelly <dankelly.6@gmail.com>
2018-03-31 07:52:55 -0400

    Refactor tax rate into a property of each category
    Also:
    * minor simmplification of discount calculation
    * extracted duplicated logic into extractTotals() function

--- katas/pricing/src/Pricing.js
+++ katas/pricing/src/Pricing.js
@@ -8,48 +8,49 @@ var Pricing = {
 
         var taxedTotals = applyTaxes(discountedTotals);
 
-        return Math.round(sum(Object.values(taxedTotals)));
-
+        return Math.round(sum(extractTotals(taxedTotals)));
 
         function sum(integerArray){
             return integerArray.reduce( (accumulator, currentValue) => accumulator + currentValue );
         }
 
+        function extractTotals(categoriesObject){
+            return Object.values(categoriesObject).map(categoryProperties => categoryProperties.total);
+        }
+
         function transformSubmittedItems(submittedItems){
-            var totalsByCategory = {
-                food: 0,
-                alcohol: 0,
-                other: 0
+            var categories = {
+                food: {total: 0, taxRate: 0},
+                alcohol: {total:0, taxRate: 0.155},
+                other: {total: 0, taxRate: 0.075}
             };
 
             submittedItems.forEach(function(item){
-                var pair = Object.entries(item).pop();
-                totalsByCategory[pair[0]] += pair[1];
+                var listItem = Object.entries(item).pop();
+                categories[listItem[0]].total += listItem[1];
             });
 
-            return totalsByCategory;
+            return categories;
         }
 
-        function applyDiscounts(totalsByCategory){
+        function applyDiscounts(categories){
             var discount = 0;
-            var preDiscountTotal = sum(Object.values(totalsByCategory));
+            var preDiscountTotal = sum(extractTotals(categories));
 
-            if(preDiscountTotal >= 10000 && preDiscountTotal < 100000){
-                discount = 0.1;
-            } else if(preDiscountTotal >= 100000){
-                discount = 0.15;
-            }
+            if(preDiscountTotal >= 100000) discount = 0.15;
+            else if(preDiscountTotal >= 10000) discount = 0.1;
 
-            Object.entries(totalsByCategory).forEach(function(pair){
-                totalsByCategory[pair[0]] -= pair[1] * discount;
+            Object.entries(categories).forEach(function(pair){
+                categories[pair[0]].total -= pair[1].total * discount;
             });
 
-            return totalsByCategory;
+            return categories;
         }
 
         function applyTaxes(totalsByCategory){
-            totalsByCategory.alcohol += totalsByCategory.alcohol *= 0.155;
-            totalsByCategory.other += totalsByCategory.other *= 0.075;
+            Object.entries(totalsByCategory).forEach(function(pair){
+                totalsByCategory[pair[0]].total += pair[1].total *= pair[1].taxRate;
+            });
             return totalsByCategory;
         }
     }
