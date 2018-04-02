ce1e729faa605d11aa5e164f32ce7a2a5e985684
Dan Kelly <dankelly.6@gmail.com>
2018-04-01 21:52:59 -0400

    Fix wording of test cases and add another test

--- katas/translator/spec/TranslatorSpec.js
+++ katas/translator/spec/TranslatorSpec.js
@@ -21,23 +21,23 @@ describe("A translator", function() {
         });
     });
 
-    describe("directed to translate a single-character, uppercase string", function () {
+    describe("directed to translate a single, uppercase character", function () {
         var result =  Translator.camelToSnake("S");
-        it("produces that same string", function () {
+        it("produces the lowercase version of that character", function () {
             expect(result).toEqual("s");
         });
     });
 
-    describe("directed to translate a multi-character, all-lowercase string", function () {
+    describe("directed to translate the multi-character, all-lowercase string 'zyxwvutsrqponmlkjihgfedcba'", function () {
         var result =  Translator.camelToSnake("zyxwvutsrqponmlkjihgfedcba");
         it("produces that same string", function () {
             expect(result).toEqual("zyxwvutsrqponmlkjihgfedcba");
         });
     });
 
-    describe("directed to translate a multi-character, all-uppercase string", function () {
+    describe("directed to translate the multi-character, all-uppercase string 'OMGASNAFU'", function () {
         var result =  Translator.camelToSnake("OMGASNAFU");
-        it("produces a version of that string consisting entirely of lowercase characters, each separated by a single underscore", function () {
+        it("produces 'o_m_g_a_s_n_a_f_u", function () {
             expect(result).toEqual("o_m_g_a_s_n_a_f_u");
         });
     });
@@ -59,7 +59,14 @@ describe("A translator", function() {
     describe("directed to translate 'aSimpleName'", function(){
         var result = Translator.camelToSnake("aSimpleName");
         it("produces 'a_simple_name'", function(){
-            expect(result).toEqual("");
+            expect(result).toEqual("a_simple_name");
+        })
+    });
+
+    describe("directed to translate 'ANextTestCase'", function(){
+        var result = Translator.camelToSnake("ANextTestCase");
+        it("produces 'a_next_test_case'", function(){
+            expect(result).toEqual("a_next_test_case");
         })
     });
 
