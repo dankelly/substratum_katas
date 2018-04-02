81d6e9177c20f138e20d9840f6f5b8dc2b430713
Dan Kelly <dankelly.6@gmail.com>
2018-04-02 13:32:07 -0400

    Use recursive solution for translator kata

--- katas/translator/src/Translator.js
+++ katas/translator/src/Translator.js
@@ -1,6 +1,20 @@
 var Translator = {
 
     camelToSnake: function(string){
-        return string;
+
+        return stripLeadingUnderscore( convert(string) );
+
+        function convert(string){
+            if(!string) return "";
+            var result = string[0];
+            if(result.match(/[A-Z]/)) result = "_" + result.toLowerCase();
+            result += convert(string.slice(1, string.length));
+            return result;
+        }
+
+        function stripLeadingUnderscore(string){
+            if(string[0] === '_') return string.slice(1, string.length);
+            else return string;
+        }
     }
 };
