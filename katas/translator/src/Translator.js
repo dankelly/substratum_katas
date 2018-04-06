b34fe5a0d846fcbd7093cc8640f420a170531633
Dan Kelly <dankelly.6@gmail.com>
2018-04-06 14:25:24 -0400

    Translator: Consolidate logic into fewer functions

--- katas/translator/src/Translator.js
+++ katas/translator/src/Translator.js
@@ -4,8 +4,13 @@ var Translator = {
 
         function preProcess(string){
             var flaggedForEndConversion = string.match(/[A-Z]{2}$/);
-            string = convertMiddleCapsForCapsSubstringsOfLengthThreeOrGreater(string);
-            if(flaggedForEndConversion) string = convertEndWhenPartOfAbbreviation(string);
+            var regex = /([A-Z])([A-Z]+)([A-Z])/;
+            var match = string.match(regex);
+            while(match){
+                string = string.replace(match[1]+match[2]+match[3], match[1]+match[2].toLowerCase()+match[3]);
+                match = string.match(regex);
+            }
+            if(flaggedForEndConversion) string = string.replace(/[A-Z]$/, string.slice(string.length-1).toLowerCase());
             return string;
         }
 
@@ -18,46 +23,10 @@ var Translator = {
         }
 
         function postProcess(string){
-            string = stripLeadingUnderscore(string);
-            string = stripUnderscoresBetweenDigits(string);
-            //string = stripUnderscoresSeparatingAbbreviations(string);
-            //string = stripUnderscoresBetweenSingleLetters(string);
-            return string;
-        }
-
-        function convertMiddleCapsForCapsSubstringsOfLengthThreeOrGreater(string){
-            var regex = /([A-Z])([A-Z]+)([A-Z])/;
-            var match = string.match(regex);
-            while(match){
-                string = string.replace(match[1]+match[2]+match[3], match[1]+match[2].toLowerCase()+match[3]);
-                match = string.match(regex);
-            }
-            return string;
-        }
-
-        function convertEndWhenPartOfAbbreviation(string){
-            return string.replace(/[A-Z]$/, string.slice(string.length-1).toLowerCase());
-        }
-
-        function stripLeadingUnderscore(string){
             if(string[0] === '_') string = string.slice(1, string.length);
-            return string;
-        }
-
-        function stripUnderscoresBetweenDigits(string){
             return string.replace(/(?<=\d)_(?=\d)/g, "");
         }
 
-        // function stripUnderscoresSeparatingAbbreviations(string){
-        //     return string.replace(/(?<=[a-z]{2,})_([a-z])_(?=[a-z]{2,})/, "$1");
-        // }
-        //
-        // function stripUnderscoresBetweenSingleLetters(string){
-        //     return string.replace(/(?<!\d){2,}_([a-z])_(?!\d){2,}/g, "$1");
-        //
-        // }
-
-        // string = preProcess(string);
         return postProcess( convert (preProcess(string) ) );
     }
 };
